847ae8a18c0f875d31ca5e0894e1673b195fc98e
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-12-26 01:29:25 +0800

    scripts/customize: handle no-action managers

--- module/customize.sh
+++ module/customize.sh
@@ -9,3 +9,11 @@ echo $MODDIR
 
 } > $MODPATH/customize.txt
 
+# action.sh
+# ap 10927 
+# ksu 11981
+if { [ "$KSU" = "true" ] && [ "$KSU_VER_CODE" -lt 11981 ]; } || 
+	{ [ "$APATCH" = "true" ] && [ "$APATCH_VER_CODE" -lt 10927 ]; }; then
+	echo "old manager - no action.sh support"
+	rm $MODPATH/action.sh
+fi
