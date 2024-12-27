13a88d5000238bbee2be54c6d06cb27e36a2fcc2
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-12-27 19:04:45 +0800

    scripts/action: sync to recent changes
    kernelsu and mmrl now allows window clear

--- module/action.sh
+++ module/action.sh
@@ -1,11 +1,40 @@
 #!/bin/sh
 # action.sh
 
-# functions
-dump_printenv() {
-	echo "[%] dumping printenv"
-	printenv | tee /data/adb/modules/detect_environ/action.txt
-}
+# mmrl's custom feature
+# https://github.com/DerGoogler/MMRL/blob/master/docs/INSTALLER.md#clear-terminal
+if [ "$MMRL" = "true" ]; then
+        if echo "$CLASSPATH" | grep -q "mmrl.debug" > /dev/null 2>&1; then
+                clear() { am broadcast -a com.dergoogler.mmrl.debug.CLEAR_TERMINAL > /dev/null 2>&1; }
+        else
+                clear() { am broadcast -a com.dergoogler.mmrl.CLEAR_TERMINAL > /dev/null 2>&1; }
+        fi
+fi
+
+# loading scren demos
+# https://github.com/tiann/KernelSU/pull/2307 
+# now live on ksu and ksu-next
+if [ "$MMRL" = "true" ] || { [ "$KSU" = "true" ] && [ "$KSU_VER_CODE" -ge 11998 ]; } \
+        || { [ "$KSU_NEXT" = "true" ] && [ "$KSU_VER_CODE" -ge 12144 ]; }; then
+        loops=20
+        while [ $loops -gt 1 ];  do 
+		for i in '[-]' '[/]' '[|]' '[\]'; do 
+		        echo "$i"
+		        sleep 0.1
+		        clear
+		        loops=$((loops - 1)) 
+		done
+        done
+else
+	# bindhosts illusion func
+	loops=20
+	while [ $loops -gt 1 ] ; do
+		echo '[.]'
+		sleep 0.1
+		loops=$((loops - 1))
+	done
+fi
+echo "[+] loading screen demo done!" 
 
 # account multiple root
 manager_daemon_count=0
@@ -14,32 +43,35 @@ manager_daemon_count=0
 [ -f /data/adb/magisk/magisk ] && manager_daemon_count=$((manager_daemon_count + 1))
 
 if [ $manager_daemon_count -gt 1 ]; then
-	echo "[!] multiple root?"
+        echo "[!] multiple root?"
 fi
 
 # echo mmrl 
 if [ -n "$MMRL" ]; then
-	echo "[+] running inside MMRL"
+        echo "[+] running inside MMRL"
 fi
 
 if [ "$APATCH" = "true" ]; then
-	echo "[+] root is APatch "
-	echo "[+] $APATCH_VER / $APATCH_VER_CODE"
-	[ "$APATCH_BIND_MOUNT" = "true" ] && echo "[+] running in bind_mount mode" 
-	dump_printenv
-	[ -z "$MMRL" ] && sleep 10
+        echo "[+] root is APatch "
+        echo "[+] $APATCH_VER / $APATCH_VER_CODE"
+        [ "$APATCH_BIND_MOUNT" = "true" ] && echo "[+] running in bind_mount mode" 
 fi
 
 if [ "$KSU" = "true" ]; then
-	echo "[+] root is KernelSU"
-	echo "[+] $KSU_KERNEL_VER_CODE / $KSU_VER_CODE"
-	[ "$KSU_MAGIC_MOUNT" = "true" ] && echo "[+] running in magic_mount mode" 
-	dump_printenv
-	[ -z "$MMRL" ] && sleep 10
+        echo "[+] root is KernelSU"
+        echo "[+] $KSU_KERNEL_VER_CODE / $KSU_VER_CODE"
+        [ "$KSU_MAGIC_MOUNT" = "true" ] && echo "[+] running in magic_mount mode" 
 fi
 
 if [ -n "$MAGISKTMP" ]; then
-	echo "[+] root is Magisk"
-	echo "[+] $(/data/adb/magisk/magisk -V) "
-	dump_printenv
+        echo "[+] root is Magisk"
+        echo "[+] $(/data/adb/magisk/magisk -V) "
+fi
+
+echo "[%] dumping printenv"
+printenv | tee /data/adb/modules/detect_environ/action.txt
+
+# APatch and KernelSU needs this
+if [ -z "$MMRL" ] && { [ "$KSU" = "true" ] || [ "$APATCH" = "true" ]; }; then
+	sleep 5
 fi
